12f6a0726c458c92de2537ad2067f44c23a42cf5
Yasu Watanabe <ywataywatay@gmail.com>
2022-02-18 09:34:58 +0900

    Insert weight to a default file.

--- site-lisp/weight-helper.el
+++ site-lisp/weight-helper.el
@@ -2,16 +2,22 @@
 (defun format-datetime ()
   (format-time-string "%Y-%m-%d %H:%M"))
 
-(defun insert-weight (weight)
+(defun weight-insert (weight)
   "Insert weight with timestamp"
   (interactive "nWeight: ")
   (let
       ((time (format-datetime)))
     (goto-char (point-max))
     (insert time "," (format "%.2f" weight) "\n")))
-
-
-
-
-
-
+(setq weight-file (expand-file-name "~/Memo/weight.csv"))
+(defun weight-insert-default-file (weight)
+  "Insert weight with timestamp to file"
+  (interactive "nWeight: ")
+  (if (boundp 'weight-file)
+    (let
+	((time (format-datetime)))
+      (save-excursion
+	(find-file weight-file)
+	(goto-char (point-max))
+	(insert time "," (format "%.2f" weight) "\n")
+	(save-buffer)))))
